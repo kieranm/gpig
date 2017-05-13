2ea9709f2ce3734be9d205a78a30d031ad10e71d
Kieran McHugh <me+github@kieranm.ch>
2017-05-13 17:16:46 +0100

    Reverting to old structure

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(__dirname));
 
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/dist/index.html');
+    response.sendFile(__dirname + '/index.html');
 });
 
 app.listen(PORT, error => {
-  error
-  ? console.error(error)
-  : console.info(`==> 🌎 Listening on port ${PORT}. Visit http://localhost:${PORT}/ in your browser.`)
+    error
+        ? console.error(error)
+        : console.info(`==> Listening on port ${PORT}. Visit http://localhost:${PORT}/ in your browser.`)
 });
