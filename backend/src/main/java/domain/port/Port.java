9b45fcebd108a3c40112e2a1d02a374c9d2ee778
sheino <pk627@york.ac.uk>
2017-05-16 17:22:42 +0100

    Adding docs to ports. Adding capabilities for ships to dock, load, unload and get new orders. 2

--- backend/src/main/java/domain/port/Port.java
+++ backend/src/main/java/domain/port/Port.java
@@ -3,7 +3,11 @@ package domain.port;
 import domain.Agent;
 import domain.util.AgentType;
 import domain.util.Carrier;
+import domain.vessel.Ship;
 import domain.world.Node;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Oliver Lea
@@ -11,17 +15,22 @@ import domain.world.Node;
 public abstract class Port extends Agent implements Carrier {
 
     private String name;
+
     private int capacity;
     private int load;
     private Node node;
 
-    public Port(AgentType agentType, String name,
-                Node node, int capacity, int load) {
+    private List<Ship> queueingShips;
+
+    private Node portNode;
+
+    public Port(AgentType agentType, String name, Node node, int capacity, int load) {
         super(agentType, node.getCoordinates());
         this.node = node;
         this.name = name;
         this.capacity = capacity;
         this.load = load;
+        this.queueingShips = new ArrayList<>();
     }
 
     public Node getNode() {
