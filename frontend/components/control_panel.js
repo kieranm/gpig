d219b606a942f7992f23f1d7a2ed23708a4a32e0
Kieran McHugh <me+github@kieranm.ch>
2017-05-16 21:27:58 +0100

    Shrunk control panel and changed to blue instead of orange

--- frontend/components/control_panel.js
+++ frontend/components/control_panel.js
@@ -4,37 +4,35 @@ import {Segment, Header, Divider, Button} from 'semantic-ui-react';
 export default class ControlPanel extends Component {
     render() {
         return(
-            <Segment color={"orange"} className={"control panel"}>
+            <Segment color={"teal"} className={"control panel"}>
                 <Header as="h1">
                     Controls
                 </Header>
                 <Divider horizontal>Simulation Speed</Divider>
                 <Button.Group fluid>
-                    <Button color={"orange"} icon={"pause"}/>
-                    <Button color={"orange"}>1x</Button>
-                    <Button color={"orange"}>5x</Button>
-                    <Button color={"orange"}>10x</Button>
-                    <Button color={"orange"}>20x</Button>
+                    <Button color={"teal"}>1x</Button>
+                    <Button color={"teal"}>10x</Button>
+                    <Button color={"teal"}>20x</Button>
                 </Button.Group>
 
                 <Divider horizontal>Mode</Divider>
                 <Button.Group fluid>
-                    <Button color={"orange"}>Legacy</Button>
-                    <Button color={"orange"}>OceanX</Button>
+                    <Button color={"teal"}>Legacy</Button>
+                    <Button color={"teal"}>OceanX</Button>
                 </Button.Group>
 
                 <Divider horizontal>Presets</Divider>
-                <Button fluid color={"orange"}>Coastal Port</Button>
-                <Button fluid color={"orange"}>Offshore Smart Port</Button>
-                <Button fluid color={"orange"}>Weather Avoidance</Button>
-                <Button fluid color={"orange"}>Road Traffic</Button>
-                <Button fluid color={"orange"}>Global Movements</Button>
+                <Button fluid color={"teal"}>Coastal Port</Button>
+                <Button fluid color={"teal"}>Offshore Smart Port</Button>
+                <Button fluid color={"teal"}>Weather Avoidance</Button>
+                <Button fluid color={"teal"}>Road Traffic</Button>
+                <Button fluid color={"teal"}>Global Movements</Button>
 
                 <Divider horizontal>Map Style</Divider>
                 <Button.Group fluid>
-                    <Button color={"orange"}>Satellite</Button>
-                    <Button color={"orange"}>Light</Button>
-                    <Button color={"orange"}>Dark</Button>
+                    <Button color={"teal"}>Sat</Button>
+                    <Button color={"teal"}>Light</Button>
+                    <Button color={"teal"}>Dark</Button>
                 </Button.Group>
 
             </Segment>
