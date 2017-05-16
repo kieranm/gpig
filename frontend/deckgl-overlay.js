bfd32fb95905909d9a5313f9853541105979c4d5
Andrei Zisu <matzipan@gmail.com>
2017-05-16 16:27:39 +0100

    Revert "Revert "Put components into root""
    This reverts commit 5d8fa3f364c1fe64d3db6314d19e4b3c892556c8.

--- frontend/deckgl-overlay.js
+++ frontend/deckgl-overlay.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import DeckGL from 'deck.gl';
+import DeckGL, {HexagonLayer} from 'deck.gl';
 
 import ShipsLayer from './components/ships-layer';
 
@@ -31,9 +31,9 @@ export default class DeckGLOverlay extends Component {
   }
 
   render() {
-    const {viewport, agents, time} = this.props;
+    const {viewport, coastal_ports, agents, time} = this.props;
 
-    if (!agents) {
+    if (!coastal_ports || !agents) {
       return null;
     }
 
@@ -43,7 +43,17 @@ export default class DeckGLOverlay extends Component {
         data: agents,
         getColor: d => d.vendor === 0 ? [253, 128, 93] : [23, 184, 190],
         opacity: 0.4
+      }),
+
+      new HexagonLayer({
+          id: 'coastal_ports',
+          data: coastal_ports,
+          getPosition: e => e.COORDINATES,
+          extruded: true,
+          elevationRange: [0, 10000],
+          radius: 500
       })
+
     ];
 
     return (
