6bd04c767c39a5085d6bd1668d459d0c9b2df429
Kieran McHugh <me+github@kieranm.ch>
2017-05-22 17:16:01 +0100

    New port design

--- frontend/components/deckgl-overlay.js
+++ frontend/components/deckgl-overlay.js
@@ -3,6 +3,15 @@ import DeckGL, {PolygonLayer} from 'deck.gl';
 
 import ShipsLayer from './ships-layer';
 
+const LIGHT_SETTINGS = {
+    lightsPosition: [0, 53.32463, 300000],
+    ambientRatio: 0.05,
+    diffuseRatio: 0.6,
+    specularRatio: 0.8,
+    lightsStrength: [2.0, 0.0, 0.0, 0.0],
+    numberOfLights: 1
+};
+
 export default class DeckGLOverlay extends Component {
 
     static get defaultViewport() {
@@ -22,9 +31,18 @@ export default class DeckGLOverlay extends Component {
     }
 
     render() {
-        const {viewport, ships, coastal_ports} = this.props;
+        const {
+            viewport,
+            ships,
+            portBases,
+            northEastPortBars,
+            southEastPortBars,
+            southWestPortBars,
+            northWestPortBars
+        } = this.props;
 
         const layers = [
+            // Ship spark-line layer
             new ShipsLayer({
                 id: 'ships',
                 data: ships,
@@ -32,14 +50,61 @@ export default class DeckGLOverlay extends Component {
 
             }),
 
+            // Port Bases Layer
             new PolygonLayer({
-                id: 'coastal_ports',
-                data: coastal_ports,
-                getFillColor: d => [23, 184, 190],
+                id: 'portBases',
+                data: portBases,
+                getFillColor: d => [150, 150, 150],
                 extruded: true,
                 filled: true,
-                getElevation: d => d.height
-            })
+                getElevation: d => d.height,
+                lightSettings: LIGHT_SETTINGS,
+            }),
+
+            // North East Port Bars
+            new PolygonLayer({
+                id: 'northEastPortBars',
+                data: northEastPortBars,
+                getFillColor: d => [202, 0, 32],
+                extruded: true,
+                filled: true,
+                getElevation: d => d.height,
+                lightSettings: LIGHT_SETTINGS
+            }),
+
+            // South East Port Bars
+            new PolygonLayer({
+                id: 'southEastPortBars',
+                data: southEastPortBars,
+                getFillColor: d => [5, 113, 176],
+                extruded: true,
+                filled: true,
+                getElevation: d => d.height,
+                lightSettings: LIGHT_SETTINGS
+            }),
+
+            // South West Port Bars
+            new PolygonLayer({
+                id: 'southWestPortBars',
+                data: southWestPortBars,
+                getFillColor: d => [166, 217, 106],
+                extruded: true,
+                filled: true,
+                getElevation: d => d.height,
+                lightSettings: LIGHT_SETTINGS
+            }),
+
+            // North West Port Bars
+            new PolygonLayer({
+                id: 'northWestPortBars',
+                data: northWestPortBars,
+                getFillColor: d => [123, 50, 148],
+                extruded: true,
+                filled: true,
+                getElevation: d => d.height,
+                lightSettings: LIGHT_SETTINGS
+            }),
+
         ];
 
         return (
